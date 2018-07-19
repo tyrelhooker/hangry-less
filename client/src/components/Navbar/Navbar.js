8cb8c8a24a0936fbab73ce938f90093cd88a61c2
T.J. Hooker <tyrelhooker@gmail.com>
2018-07-19 12:20:15 -0400

    work on links in navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = () => (
       <div className="nav-wrapper">
         <a href="/" className="brand-logo">Logo</a>
         <ul className="right hide-on-med-and-down">
-          <li><a href="/Recipes">Recipes</a></li>
-          <li><a href="/MyPantry">My Pantry</a></li>
-          <li><a href="/GroceryList">Grocery List</a></li>
-          <li><a href="/SingleRecipe">Single Recipe</a></li>
+          <li><a href="/Recipes" className="hoverable">Recipes</a></li>
+          <li><a href="/MyPantry" className="hoverable">My Pantry</a></li>
+          <li><a href="/GroceryList" className="hoverable">Grocery List</a></li>
+          <li><a href="/SingleRecipe" className="hoverable">Single Recipe</a></li>
         </ul>
       </div>
     </nav>
