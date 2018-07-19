ec65fbfc084aed8afc4409a2a5071246c9f60a2a
T.J. Hooker <tyrelhooker@gmail.com>
2018-07-19 11:26:25 -0400

    create grid components and Navbar.js. Start on Main.js

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => (
+  <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">Logo</a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+);
+
+export default Navbar;
---
+++ client/src/pages/Main/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Navbar from "../../components/Navbar";
+import { Col, Row, Container } from "../../components/Grid";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Container fluid>
+          <Row>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
+}
