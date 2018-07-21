42bc8f40b77e521d61b49b25009995dc3fe7f127
unknown <jlbarbut@ncsu.edu>
2018-07-21 11:02:25 -0400

    utils folder added

---
+++ client/src/utils/API.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+//Do we want the save and delete functions to point to a new collection?
+//They need to be linked with the user's account...
+
+export default {
+  //Get all recipes from mongoDB
+  getRecipes: function() {
+    return axios.get("/api/recipes");
+  },
+  //Get recipe by specific id
+  getRecipe: function(id) {
+    return axios.get("/api/recipes/" + id);
+  },
+  //Delete recipe by specific id
+  deleteRecipe: function(id) {
+    return axios.delete("/api/recipes/" + id);
+  },
+  //Save a recipe by specific id
+  saveRecipe: function(id) {
+    return axios.post("/api/recipes/" + id);
+  }
+}
