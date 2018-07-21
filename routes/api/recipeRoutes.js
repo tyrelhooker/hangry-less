8b5bb6e103a82894912945d2db0e595a4730823e
unknown <jlbarbut@ncsu.edu>
2018-07-21 10:21:10 -0400

    working server and mongo connection

--- routes/api/recipeRoutes.js
+++ routes/api/recipeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const recipeController = require("../contoller/recipeController");
+const recipeController = require("../../controller/recipeController");
 
 //Matches with "api/recipes" --- see ./index.js and ../index.js
 router.route("/")
