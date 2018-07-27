7fa181e7d2d27953ff5b797e8d71831b9b5975be
T.J. Hooker <tyrelhooker@gmail.com>
2018-07-27 00:04:07 -0400

    format RecipeCard.js to have uniform heights and added styling

--- client/src/components/Card/RecipeCard.js
+++ client/src/components/Card/RecipeCard.js
@@ -4,9 +4,9 @@ import { Col } from "../../components/Grid";
 import "./RecipeCard.css";
 
 export const RecipeCard = props => (
-  <div className="card recipeCard">
-    <Link to={`/ExpandedRecipe/${props.dataId}`}>
-      <Col size="s12 m6 l4">
+  <Link to={`/ExpandedRecipe/${props.dataId}`}>
+    <Col size="s12 m6 l4">
+      <div className="card recipeCard">
         <div className="card-image">
           <img src={props.image} />
           {/* <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">+</i></a> */}
@@ -14,9 +14,9 @@ export const RecipeCard = props => (
         <div className="card-content">
           <h4 className="black-text" data-id={props.dataId}>{props.title}</h4>
         </div>
-      </Col>
-    </Link>
-  </ div>
+      </ div>
+    </Col>
+  </Link>
 );
 
 // export default RecipeCard;
