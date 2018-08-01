dc797f6fd7cbaacd4a18672641e292f2fb77c7bf
T.J. Hooker <tyrelhooker@gmail.com>
2018-08-01 12:44:50 -0400

    add _target to hello fresh link

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => (
   <footer className="page-footer green darken-1">
     <div className="footer-copyright">
       <div className="container center"> 
-      <p><a className="grey-text text-lighten-3" href="https://www.hellofresh.com/">All recipes are from Hello Fresh. Click for more recipes!</a> </p>
+      <p><a className="grey-text text-lighten-3" href="https://www.hellofresh.com/" target="_blank">All recipes are from Hello Fresh. Click for more recipes!</a> </p>
       Hangry-less is a JMT Design | © 2018 Copyright 
       </div>
     </div>
